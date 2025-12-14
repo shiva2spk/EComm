5cdefecc67a96968f33a1e69144a9c072bc0a7c5
Shiva SIngh <167636369+shiva2spk@users.noreply.github.com>
2025-12-14 17:01:52 +0530

    Delete mcomm directory

--- mcomm/app/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(home)/page.tsx
-export default function HomePage() {
-  return (
-    <div>
-      {/* yahan home ka content, categories, featured products, etc. */}
-      Home page content
-    </div>
-  );
-}
